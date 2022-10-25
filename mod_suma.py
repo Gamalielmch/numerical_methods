726ed4e0ad2c52c36b75b1278fd149648bea3c36
Gamaliel <55702349+Gamalielmch@users.noreply.github.com>
2022-10-25 09:03:35 -0500

    update

---
+++ mod_suma.py
@@ -0,0 +1,63 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "2bbbdf7d-7879-4406-ba71-9da4e749d5df",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def suma_dos(a,b):\n",
+    "    return(a+b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "8a5b9b7e-997a-4849-be12-e4c6287b6ce6",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def resta_dos(a,b):\n",
+    "    return(a-b)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "353a26dc-8f9e-477e-8d50-eabd631c90ee",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "88482914-ff6d-453c-81c7-d0b3228aa51d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
